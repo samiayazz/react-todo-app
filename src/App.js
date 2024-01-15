fb922e6617b93ee24bcbca56eaa46cfa5cd092a4
samiayazz <ayazzsami@gmail.com>
2024-01-15 20:03:31 +0300

    Add Todos page with Bootstrap and Font-Awesome

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 
 class App extends React.Component {
     constructor() {
         super();
 
         this.state = {
-            text: 'Hello, world!'
+            todos: [
+                { id: 1, desc: 'First todo.' },
+                { id: 2, desc: 'Second todo.' },
+                { id: 3, desc: 'Third todo.' }
+            ]
         }
     }
 
@@ -16,9 +22,42 @@ class App extends React.Component {
     render() {
         return (
             <div className='App'>
-                <h1>{this.state.text}</h1>
+                <div className='col-6 m-auto mt-5'>
+                    <header className='container-fluid d-flex justify-content-between mb-5'>
+                        <h1>Todos</h1>
+                        <button className='btn btn-dark'>Add todo</button>
+                    </header>
+
+                    <ul className='list-group bg-dark'>
+                        {
+                            this.state.todos.map(todo =>
+                                <li key={todo.id}
+                                    className='list-group-item bg-dark text-light d-flex justify-content-between px-3'>
+                                    <span style={{ alignSelf: 'center' }}>{todo.desc}</span>
+
+                                    <div className='btn-group'>
+                                        <button className='btn btn-dark text-info'>
+                                            <FontAwesomeIcon icon={faPenToSquare} />
+                                        </button>
+                                        <button className='btn btn-dark text-danger' onClick={this.removeTodoById(todo.id)}>
+                                            <FontAwesomeIcon icon={faTrash} />
+                                        </button>
+                                    </div>
+                                </li>
+                            )
+                        }
+                    </ul>
+
+                    <div className='container-fluid d-flex justify-content-end mt-5'>
+                        <button className='btn btn-dark text-danger'>Clear all</button>
+                    </div>
+                </div>
             </div>
         );
+    }
+
+    removeTodoById(id) {
+
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import App from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
